ed25aa07d5d7ad878c0ff859ef6e5cf4a2098d9f
Ohad1997 <33723454+Ohad1997@users.noreply.github.com>
2019-02-01 03:04:06 -1000

    Update google_CrawlerFaces.py

--- google_CrawlerFaces.py
+++ google_CrawlerFaces.py
@@ -14,38 +14,23 @@ from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 import time
 
-# Libraries imported to find faces
-import cv2
-import dlib
-
-SCALE_FACTOR = 0.6 # Determines the resize amount of the image when its processed, bigger= better detection but slower
-detector = dlib.get_frontal_face_detector()
 directory = os.path.join(os.path.dirname(os.path.abspath(__file__)),"Images") # Make a new folder called "Images" in the current folder
-requestPool= mp.cpu_count() * 3 # Determines the amount of proccesses working simultaneously for sending requests to download images
+requestPool= mp.cpu_count() * 6 # Determines the amount of proccesses working simultaneously for sending requests to download images
 session = requests.Session() # new session of requests
+searchtext = "face" # The search query
 
-def findFaces(link):
+def downloadImg(link):
+    print(f"downloading: {link}")
     try:
-        r=session.get(link, allow_redirects=False, timeout=4).content
-        nparr = np.fromstring(r, np.uint8) 
-        gray = cv2.imdecode(nparr, 0)
-        if isinstance(gray,np.ndarray):
-            gray = cv2.resize(gray, (int(gray.shape[1] * SCALE_FACTOR),int(gray.shape[0] * SCALE_FACTOR))) 
-            rects = detector(gray, 1)
-            if rects:
-                fname = os.path.join(directory, link.split('/')[-1])
-                with open(fname, 'wb') as f:
-                    f.write(r)
-                    return link
-        return None
+        r = session.get(link, allow_redirects=False, timeout=4).content
+        fname=os.path.join(os.path.dirname(os.path.abspath(__file__)),"images",link.split('/')[-1])
+        with open(fname, 'wb') as f:
+            f.write(r)
     except Exception as e:
-        print(e)
-        return None
-        
+        print("Download failed:", e)
+
 def sliceSource(browser):
-    res = [value for value in (imtype(a) for a in browser.find_elements_by_class_name("rg_meta"))  if value]
-    with mp.Pool(requestPool) as p: # Workers downloading the Images simultaneously
-        return p.map(findFaces, [url for url in res if url])
+    return [value for value in (imtype(a) for a in browser.find_elements_by_class_name("rg_meta"))  if value]
 
 def imtype(a):
     ja=json.loads(a.get_attribute("innerHTML"))
@@ -58,7 +43,7 @@ def imtype(a):
         return link
     return None
 
-def openUrl(browser,searchtext):
+def openUrl(browser):
     url = "https://www.google.com/search?q="+searchtext+"&source=lnms&tbm=isch"
     # Open the link
     browser.get(url)
@@ -75,7 +60,7 @@ def openUrl(browser,searchtext):
             browser.find_element_by_id("smb").click() # Click on "show more images" button
         except Exception:
             pass
-        counter+=10
+        counter+=5
 
 
 def extended_openUrl(browser,imgUrl):
@@ -97,7 +82,6 @@ def extended_openUrl(browser,imgUrl):
     
 #------------- Main Program -------------#
 def main():
-    searchtext = "face" # The search query
     sTime = time.time()
     options = webdriver.ChromeOptions()
     
@@ -120,12 +104,15 @@ def main():
                 "installed on your machine (exception: %s)" % e)
         sys.exit()
 
-    openUrl(browser,searchtext) # Get page source
+    openUrl(browser) # Get page source
 
     if not os.path.exists(directory): # If folder "Images" doesnt exist, create it
         os.makedirs(directory)
 
     actualImages=sliceSource(browser) # Divides the Images urls
+    with mp.Pool(requestPool) as p: # Workers downloading the Images simultaneously
+        return p.map(downloadImg, [url for url in actualImages if url])
+
     img_total=len(actualImages)
 
     for imgUrl in actualImages:
@@ -137,6 +124,8 @@ def main():
         if not success:
             continue
         secondaryImages=sliceSource(browser)# Divides the image urls
+        with mp.Pool(requestPool) as p: # Workers downloading the Images simultaneously
+            return p.map(downloadImg, [url for url in secondaryImages if url])
         img_total+=len(secondaryImages)
         #copy paste this part to loop as many times as u want
 
